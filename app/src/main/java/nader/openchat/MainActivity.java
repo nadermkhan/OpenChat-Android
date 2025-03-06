e7e708dd005fdbe58cc4d2e1370fdccd5d9d05c1
nadermkhan <muhammadnadermahbubkhan@gmail.com>
2025-03-06 22:44:40 +0600

    update MainActivity.java

--- app/src/main/java/nader/openchat/MainActivity.java
+++ app/src/main/java/nader/openchat/MainActivity.java
@@ -18,6 +18,7 @@ import nader.openchat.SharedPrefManager;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 	
